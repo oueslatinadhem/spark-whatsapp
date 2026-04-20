c35b22c7931f23cf09d86d748e1d55ae5e11de1b
Nadhem Oueslati <oueslati.nadhem@gmail.com>
2026-04-20 13:17:02 +0000

    version 4.98.0

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ ED_USERNAME = os.environ.get("ED_USERNAME")
 ED_PASSWORD = os.environ.get("ED_PASSWORD")
 
 FA = [
-    {"cn": "ED_UExVTUVfMDc1MjkxMUdfMV8xMDg0Mg==", "cv": "NzM0NDUwMzU0YTMyNGY2Yzc3NGIzMDcyMzI0ZTY2NjU0MTYxNDQ0ZjVhNTk3NzRjNDQ0NTMwNDY1MzQx"},
-    {"cn": "ED_UExVTUVfMDc1MjkxMUdfRV85NTU5", "cv": "NTk2ZDc3NGU0MzZmNDQ0NzRhNGUzMjU2MzE3OTM3Njk3ODJiNDM3MjU0MzE1MzY3NGY2YjZiMmY="},
-    {"cn": "ED_UExVTUVfMDc1MjkxMUdfMV8xMDg0Mw==", "cv": "NjQ2ZDU1NDI1MTJiNzM3ODRhNDE3NDQ2NTI0YTU4NzY1NTRiNmM2MjM0NDE1NzU3NmYzNTQyNGY2ODUx"}
+    {"cn": "ED_UExVTUVfMDc1MjkxMUdfMV8xMDg0Mg==", "cv": "NzM0NDUwMzU0YTMyNGY2Yzc3NGIzMDcyMzI0ZTY2NjU0MTYxNDQ0ZjVhNTk3NzRjNDQ0NTMwNDY1MzQx", "uniq": False},
+    {"cn": "ED_UExVTUVfMDc1MjkxMUdfRV85NTU5", "cv": "NTk2ZDc3NGU0MzZmNDQ0NzRhNGUzMjU2MzE3OTM3Njk3ODJiNDM3MjU0MzE1MzY3NGY2YjZiMmY=", "uniq": False},
+    {"cn": "ED_UExVTUVfMDc1MjkxMUdfMV8xMDg0Mw==", "cv": "NjQ2ZDU1NDI1MTJiNzM3ODRhNDE3NDQ2NTI0YTU4NzY1NTRiNmM2MjM0NDE1NzU3NmYzNTQyNGY2ODUx", "uniq": False}
 ]
 
 @app.route('/test_login')
@@ -22,16 +22,21 @@ def test_login():
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
         "Content-Type": "application/x-www-form-urlencoded"
     }
-    session.get("https://api.ecoledirecte.com/v3/login.awp?gtk=1&v=4.75.0", headers=headers)
+    # Version 4.98.0 comme le vrai navigateur
+    session.get("https://api.ecoledirecte.com/v3/login.awp?gtk=1&v=4.98.0", headers=headers)
     login_data = {
         "identifiant": ED_USERNAME,
         "motdepasse": ED_PASSWORD,
-        "isRelogin": False,
+        "isReLogin": False,
         "uuid": "",
         "fa": FA
     }
     payload = "data=" + quote(json.dumps(login_data))
-    resp = session.post("https://api.ecoledirecte.com/v3/login.awp?v=4.75.0", headers=headers, data=payload)
+    resp = session.post(
+        "https://api.ecoledirecte.com/v3/login.awp?v=4.98.0",
+        headers=headers,
+        data=payload
+    )
     return jsonify(resp.json())
 
 if __name__ == '__main__':
